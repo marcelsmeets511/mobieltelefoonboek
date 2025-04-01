6f6d7bca544c1992e2575a8497b4a1ae9ef90a30
marcelsmeets511 <marcelsmeets511@gmail.com>
2025-04-01 19:28:33 +0200

    update app.py

--- app.py
+++ app.py
@@ -14,11 +14,11 @@ load_dotenv()
 app = Flask(__name__)
 
 # Database connection parameters
-DB_HOST = os.getenv('DB_HOST', DB_HOST)
-DB_PORT = os.getenv('DB_PORT', DB_PORT)
-DB_NAME = os.getenv('DB_NAME', DB_NAME)
-DB_USER = os.getenv('DB_USER', DB_USER)
-DB_PASSWORD = os.getenv('DB_PASSWORD', 'Witches3255')
+DB_HOST = os.getenv('DB_HOST', "")
+DB_PORT = os.getenv('DB_PORT', "")
+DB_NAME = os.getenv('DB_NAME', "")
+DB_USER = os.getenv('DB_USER', "")
+DB_PASSWORD = os.getenv('DB_PASSWORD', '')
 
 def get_db_connection():
     """Create a database connection"""
